c8ce1e6a880e0989f757d3af3cf5c089c4f825f6
Adeonir Kohl <adeonir@gmail.com>
2026-02-21 23:26:12 -0300

    feat: dim parent paths in list output to highlight artifact names

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -13,7 +13,16 @@ pub fn execute() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     for path in paths {
-        println!("{path}");
+        let p = std::path::Path::new(&path);
+        match (
+            p.parent().and_then(|p| p.to_str()),
+            p.file_name().and_then(|n| n.to_str()),
+        ) {
+            (Some(parent), Some(name)) => {
+                println!("{}{name}", style(format!("{parent}/")).dim());
+            }
+            _ => println!("{path}"),
+        }
     }
 
     Ok(())
